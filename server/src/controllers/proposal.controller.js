15827393187118b935ab1cb1a4d5a14dce42584b
rafael.lapetina <rafalapetina@gmail.com>
2023-11-09 10:36:42 +0100

    get all proposals query supports all filters

--- server/src/controllers/proposal.controller.js
+++ server/src/controllers/proposal.controller.js
@@ -1,16 +1,51 @@
-import { getKeyWordsFromDB, getProposalsFromDB } from '../services/proposal.services.js';
-import { LevelsEnum } from '../models/LevelsEnum.js';
+import {
+  getKeyWordsFromDB,
+  getProposalsFromDB,
+} from "../services/proposal.services.js";
+import { LevelsEnum } from "../models/LevelsEnum.js";
+import { isValidDateFormat } from "../utils/utils.js";
 
 export const getProposals = async (req, res, next) => {
   try {
+    const cod_degree = req.query.cod_degree;
+    const start_expiration_date = req.query.start_date;
+    const end_expiration_date = req.query.end_date;
 
-    const { start_expiration_date, end_expiration_date } = req.query;
-    const { supervisor_id } = req.query;
+    if (!cod_degree) {
+      return res.status(400).json({ message: "cod_degree is required" });
+    }
+    if (start_expiration_date && isValidDateFormat(start_expiration_date) === false) {
+      return res
+        .status(400)
+        .json({
+          message: "Invalid start_expiration_date, format should be YYYY-MM-dd",
+        });
+    }
+    if (end_expiration_date && isValidDateFormat(end_expiration_date) === false) {
+      return res
+        .status(400)
+        .json({
+          message: "Invalid end_expiration_date, format should be YYYY-MM-dd",
+        });
+    }
 
-    const keyword_ids = req.query.keyword_ids ? JSON.parse(req.query.keyword_ids) : [];
-    const level_ids = req.query.level_ids ? JSON.parse(req.query.level_ids) : [];
+    const supervisor_id = req.query.supervisor_id;
 
-    const proposals = await getProposalsFromDB(level_ids, keyword_ids, supervisor_id);
+    const keyword_ids = req.query.keyword_ids
+      ? JSON.parse(req.query.keyword_ids)
+      : [];
+    const level_ids = req.query.level_ids
+      ? JSON.parse(req.query.level_ids)
+      : [];
+
+    const proposals = await getProposalsFromDB(
+      cod_degree,
+      level_ids,
+      keyword_ids,
+      supervisor_id,
+      start_expiration_date,
+      end_expiration_date
+    );
     return res.json(proposals);
   } catch (err) {
     return next(err);
---
+++ server/src/models/Proposal.js
@@ -0,0 +1,72 @@
+export class Proposal {
+  constructor(
+    id,
+    title,
+    description,
+    expiration_date,
+    cod_degree,
+    level,
+    supervisor_id,
+    notes,
+    cod_group,
+    required_knowledge
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.notes = notes;
+    this.expiration_date = expiration_date;
+    this.level = level;
+    this.cod_degree = cod_degree;
+    this.supervisor_id = supervisor_id;
+    this.cod_group = cod_group;
+    this.required_knowledge = required_knowledge;
+  }
+
+  // Used to send the Proposals object to the client
+  serialize = () => {
+    return {
+      id: this.id,
+      title: this.title,
+      description: this.description,
+      expiration_date: this.expiration_date,
+      cod_degree: this.cod_degree,
+      level: this.level,
+      supervisor_id: this.supervisor_id,
+      notes: this.notes,
+      cod_group: this.cod_group,
+      required_knowledge: this.required_knowledge,
+    };
+  };
+
+  // Used to retrieve the Proposals object from client object
+  static fromJson = (json) => {
+    return new Proposal(
+      json.id,
+      json.title,
+      json.description,
+      json.expiration_date,
+      json.cod_degree,
+      json.level,
+      json.supervisor_id,
+      json.notes,
+      json.cod_group,
+      json.required_knowledge
+    );
+  };
+
+  static fromProposalsResult = (result) => {
+    return new Proposal(
+      result.id,
+      result.title,
+      result.description,
+      result.expiration_date,
+      result.cod_degree,
+      result.level,
+      result.supervisor_id,
+      result.notes,
+      result.cod_group,
+      result.required_knowledge
+    );
+  }
+}
