cbec974557f66169245ad47dcbc3f9dab62557ed
GioGiunta2606 <s318082@studenti.polito.it>
2023-11-16 12:19:55 +0100

    fixed bug

--- client/src/services/applications.api.js
+++ client/src/services/applications.api.js
@@ -23,8 +23,27 @@ const createApplication = async (proposal_id, student_id, submission_date) => {
   }
 };
 
+const getApplicationStudentsByProposalId = async (proposalId) => {
+  try {
+      const response = await fetch(SERVER_URL + `/api/applications/proposal/${proposalId}`, {
+          method: "GET",
+      });
+      if (response.ok) {
+          const application = await response.json();
+          return application;
+      } else {
+          const message = await response.text();
+          throw new Error("Application error: " + message);
+      }
+  } catch (error) {
+      throw new Error("Network Error: " + error.message);
+  }
+
+};
+
 const applicationsAPI = {
   createApplication,
+  getApplicationStudentsByProposalId,
 };
 
 export default applicationsAPI;
