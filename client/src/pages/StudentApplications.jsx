b5805c6cf6728c84e230d1df0cb80881ea1bd665
GioGiunta2606 <s318082@studenti.polito.it>
2023-11-27 16:46:42 +0100

    added go back button in the StudentApplications page

--- client/src/pages/StudentApplications.jsx
+++ client/src/pages/StudentApplications.jsx
@@ -2,9 +2,13 @@ import { useEffect, useState } from 'react';
 import MainDashboard from '../components/MainDashboard.jsx';
 import Box from '@mui/material/Box';
 import applicationsAPI from '../services/applications.api.js';
+import { Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function StudentApplications(props) {
   const [appliedProposals, setAppliedProposals] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const retriveStudentApplications = async () => {
@@ -16,10 +20,23 @@ function StudentApplications(props) {
   }, []);
   return (
     <>
-      <Box sx={{ display: 'inline-flex' }} mt={'15vh'} mx={'6vh'}>
-        <MainDashboard proposals={appliedProposals} isAppliedProposals={true} />
-      </Box>
+      <Grid container>
+        <Grid item xs={12}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: '15vh', mx: '6vh' }}>
+            <Button onClick={() => navigate('/student')} variant="contained" startIcon={<ArrowBackIcon />}>
+              Go Back
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        <Grid item xs={12} sx={{ mt: '2vh', mx: '4vh' }}>
+          <MainDashboard proposals={appliedProposals} isAppliedProposals={true} />
+        </Grid>
+      </Grid>
     </>
+
   );
 }
 
