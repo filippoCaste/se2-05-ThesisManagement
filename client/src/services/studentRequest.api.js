a6a134e1ef1d3fc71212dc3b068f0ef6ccdffe5f
filippoCaste <filippo.castellarin@gmail.com>
2023-12-10 23:02:45 +0100

    API connection

---
+++ client/src/services/studentRequest.api.js
@@ -0,0 +1,41 @@
+"use strict";
+const SERVER_URL = "http://localhost:3001";
+
+/**
+ * 
+ * @param {Object} studentRequest 
+ * - `title`
+ * - `type`
+ * - `desciption`
+ * - `notes`
+ * - `teacherEmail`
+ * - `coSupervisorEmails[]`
+ * @returns 
+ */
+const postStudentRequest = async (studentRequest) => {
+    try {
+
+        const response = await fetch(SERVER_URL + "/api/proposals/request", {
+            method: "POST",
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(studentRequest),
+        });
+        if (response.ok) {
+            return true;
+        } else {
+            const message = await response.text();
+            throw new Error("Application error: " + message);
+        }
+    } catch (error) {
+        throw new Error("Network Error: " + error.message);
+    }
+};
+
+const studentRequestAPI = {
+    postStudentRequest,
+}
+
+export default studentRequestAPI;
