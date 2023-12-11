a8a5e31d1b56977f63e379361bd0432e4d2e5fab
filippoCaste <filippo.castellarin@gmail.com>
2023-12-11 11:37:44 +0100

    fixes

--- client/src/components/ConfirmationDialog.jsx
+++ client/src/components/ConfirmationDialog.jsx
@@ -1,5 +1,4 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import { useState } from "react";
 
 export default function ConfirmationDialog({ open, onClose, onConfirm, message, operation }) {
     // operation: 'Delete', 'Update', 'Apply', 'Send', ...
@@ -38,38 +37,3 @@ export default function ConfirmationDialog({ open, onClose, onConfirm, message, 
         </Dialog>
     );
 }
-
-
-
-
-/*
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-
-export default function ResponsiveDialog() {
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-
-
-  return (
-    <React.Fragment>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-
-      </Dialog>
-    </React.Fragment>
-  );
-}
-*/
