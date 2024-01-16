cbf104828b103675e9ce1a9bd8a572b62e3380ac
s312121 <s312121@studenti.polito.it>
2024-01-16 13:54:49 +0100

    fix disable Apply btn for student caused by miss spell

--- client/src/components/AlertDialog.jsx
+++ client/src/components/AlertDialog.jsx
@@ -216,7 +216,7 @@ export default function AlertDialog({
               onClick={() => handleApply(selectedFile)}
               color="primary"
               variant="contained"
-              disabled={isAppliedProposals}
+              disabled={isAppliedProposal}
             >
               <Typography variant="button" sx={{ color: "white" }}>
                 Apply
